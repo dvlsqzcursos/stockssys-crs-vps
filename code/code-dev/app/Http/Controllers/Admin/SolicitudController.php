2f31505ac5a946c9b0dee0b7f9d29f5d00964e08
dev-igss <danielvel1977@gmail.com>
2024-05-16 11:24:20 -0600

    Update SolicitudController.php

--- code/code-dev/app/Http/Controllers/Admin/SolicitudController.php
+++ code/code-dev/app/Http/Controllers/Admin/SolicitudController.php
@@ -1654,7 +1654,7 @@ class SolicitudController extends Controller
         ->Join('bodegas_ingresos_detalles as bi_det', 'bi_det.id_insumo', 'b.id')
         ->where('b.id_institucion', Auth::user()->id_institucion)  
         ->where('b.tipo_bodega', 1) 
-        ->where('bi_det.no_unidades', '-', 'bi_det.no_unidades_usadas', '>', '0')
+        ->where('bi_det.no_unidades', '-', 'bi_det.no_unidades_usadas', '!=', '0')
         ->orderBy('bi_det.bubd')
         ->get();
         return $saldos;
